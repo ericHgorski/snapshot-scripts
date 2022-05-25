8bce78ac946a3fa5707430d9d887118b228b736f
Eric Gorski <er.h.gorski@gmail.com>
2022-05-25 16:03:32 -0400

    cleanup

--- angel.js
+++ angel.js
@@ -1,4 +1,4 @@
-import { lcd, ANGEL_ENDOWMENT_ADDRESS, apANC_CONTRACT_ADDRESS, SNAP_BLOCK_HEIGHT } from "./utils.js";
+import { lcd, ANGEL_ENDOWMENT_ADDRESS, apANC_CONTRACT_ADDRESS, COMPARISON_BLOCK_HEIGHT } from "./utils.js";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -8,14 +8,13 @@ const main = async () => {
   const { endowments } = await lcd.wasm.contractQuery(
     ANGEL_ENDOWMENT_ADDRESS,
     { endowment_list: {} },
-    { height: SNAP_BLOCK_HEIGHT }
+    { height: COMPARISON_BLOCK_HEIGHT }
   );
-  const endowmentAddresses = [...endowments].map(({address}) => address);
+  const ANGEL_BALANCES = await Promise.all([...endowments].map(({address}) =>  getCharityData(address)));
 
-  const ANGEL_BALANCES = await Promise.all(endowmentAddresses.map((address) => getCharityData(address)));
   fs.writeFileSync(
     path.join(DATA_DIR, "angel" + ".json"),
-    JSON.stringify(ANGEL_BALANCES)
+    JSON.stringify(ANGEL_BALANCES.filter(({balance}) => balance !== 0))
   );
 }
 
@@ -24,14 +23,16 @@ const getCharityData = async (address) => {
   const { locked_cw20, liquid_cw20 } = await lcd.wasm.contractQuery(
     apANC_CONTRACT_ADDRESS, 
     { balance: { address }}, 
-    { height: SNAP_BLOCK_HEIGHT });
+    { height: COMPARISON_BLOCK_HEIGHT });
   const lockedApAnc = locked_cw20[0] ? Number(locked_cw20[0].amount) : 0;
   const liquidApAnc = liquid_cw20[0] ? Number(liquid_cw20[0].amount) : 0;
+  const balance = liquidApAnc + lockedApAnc
   
   return {
-    amount: lockedApAnc + liquidApAnc,
-    denom: "aUST", // 1 apANC === 1 aUST
-    address: owner
+    balance,
+    denom: "aUST", // 1 apANC === 1 aUST,
+    contract: address,
+    owner
   };
 }
 
--- utils.js
+++ utils.js
@@ -1,8 +1,12 @@
 import { LCDClient } from "@terra-money/terra.js";
 
 export const ANGEL_ENDOWMENT_ADDRESS = "terra1nwk2y5nfa5sxx6gtxr84lre3zpnn7cad2f266h";
-export const SNAP_BLOCK_HEIGHT = 7544510;
 export const apANC_CONTRACT_ADDRESS = "terra172ue5d0zm7jlsj2d9af4vdff6wua7mnv6dq5vp";
+
+export const PRE_ATTACK_BLOCK_HEIGHT = 7544910;
+export const POST_ATTACK_BLOCK_HEIGHT = 7790000;
+export const COMPARISON_BLOCK_HEIGHT = 7603700;
+
 export const lcd = new LCDClient({
   URL: "https://lcd.terra.dev", // use localhost if you want to speedup the data collection process
   chainID: "columbus-5",
